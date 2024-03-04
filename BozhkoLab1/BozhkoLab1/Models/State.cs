b1023ada816063fa3a9fdace5a7f4f76d482e465
youngB0rrov <fair52525@gmail.com>
2024-03-04 18:55:38 +0300

    Initial commit;

---
+++ BozhkoLab1/BozhkoLab1/Models/State.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BozhkoLab1.Models
+{
+	public class State
+	{
+		private readonly int _n = 4;
+		private readonly int _sum = 30;
+		public int G { get; set; } = 0;
+		public int H { get; set; } = 0;
+		public bool IsOpened { get; set; }
+		public State? Parent { get; set; }
+		public List<List<int>> Position { get; set; } = new();
+		public State(int g)
+		{
+			G = g;
+			Position = new List<List<int>>
+			{
+				new List<int> { 1, 2, 3, 4},
+				new List<int> { 5, 6, 7, 8},
+				new List<int> { 9, 10,11, 12},
+				new List<int> { 13, 15, 14, 0},
+			};
+			CalculateH();
+		}
+		public State(State parent, List<List<int>> position, int g)
+		{
+			G = g;
+			Position = position;
+			Parent = parent;
+			CalculateH();
+		}
+		private static State MoveRight(State parent, int g, int i, int j)
+		{
+			var newPosition = parent.Position.ConvertAll(x => new List<int>(x));
+			var temp = parent.Position[i][j + 1];
+			newPosition[i][j + 1] = 0;
+			newPosition[i][j] = temp;
+
+			return new State(parent, newPosition, g);
+		}
+		private static State MoveLeft(State parent, int g, int i, int j)
+		{
+			var newPosition = parent.Position.ConvertAll(x => new List<int>(x));
+			var temp = parent.Position[i][j - 1];
+			newPosition[i][j - 1] = 0;
+			newPosition[i][j] = temp;
+
+			return new State(parent, newPosition, g);
+		}
+		private static State MoveUp(State parent, int g, int i, int j)
+		{
+			var newPosition =	parent.Position.ConvertAll(x => new List<int>(x));
+			var temp = parent.Position[i - 1][j];
+			newPosition[i - 1][j] = 0;
+			newPosition[i][j] = temp;
+
+			return new State(parent, newPosition, g);
+		}
+		private static State MoveDown(State parent, int g, int i, int j)
+		{
+			var newPosition = parent.Position.ConvertAll(x => new List<int>(x));
+			var temp = parent.Position[i + 1][j];
+			newPosition[i + 1][j] = 0;
+			newPosition[i][j] = temp;
+
+			return new State(parent, newPosition, g);
+		}
+		private int GetColumnSum(int columnIndex)
+		{
+			var sum = 0;
+			for (var i = 0; i < _n; ++i)
+			{
+				sum += Position[i][columnIndex];
+			}
+			return sum;
+		}
+		private int GetRowSum(int rowIndex)
+		{
+			var sum = 0;
+			for (var i = 0; i < _n; ++i)
+			{
+				sum += Position[rowIndex][i];
+			}
+			return sum;
+		}
+		private Tuple<int, int> GetDiagonalsSum()
+		{
+			var leftDiagonalSum = 0;
+			var rightDiagonalSum = 0;
+
+			for (var i = 0; i < _n; ++i)
+			{
+				leftDiagonalSum += Position[i][i];
+			}
+			for (var i = _n - 1; i <= 0; --i)
+			{
+				rightDiagonalSum += Position[i][_n - 1 - i];
+			}
+			return Tuple.Create(leftDiagonalSum, rightDiagonalSum);
+		}
+		private void CalculateH()
+		{
+			var h = 0;
+
+			for (var i = 0; i < _n; ++i)
+			{
+				var columnDelta = Math.Abs(_sum - GetColumnSum(i));
+				var rowDelta = Math.Abs(_sum - GetRowSum(i));
+				h += columnDelta;
+				h += rowDelta;
+			}
+			var diagonalsSum = GetDiagonalsSum();
+			h += Math.Abs(_sum - diagonalsSum.Item1);
+			h += Math.Abs(_sum - diagonalsSum.Item2);
+
+			H = h;
+		}
+		private Tuple<int, int> GetEmptyPositionIndexes(List<List<int>> position)
+		{
+			var result = new Tuple<int, int>(0, 0);
+			for (var i = 0; i < _n; ++i)
+			{
+				for (var j = 0; j < _n; j++)
+				{
+					if (position[i][j] == 0)
+					{
+						result = Tuple.Create(i, j);
+					}
+				}
+			}
+
+			return result;
+		}
+		public bool IsAlreadyOpen(List<State> opened)
+		{
+			var currentItemDigits = this.Position.SelectMany(x => x.Select(y => y)).ToList();
+			
+			foreach (var open in opened)
+			{
+				var openItemDigits = open.Position.SelectMany(x => x.Select(y => y)).ToList();
+				var equalDigitsCount = 0;
+				for (var i = 0; i < _n * _n; ++i)
+				{
+					if (openItemDigits[i] == currentItemDigits[i])
+					{
+						equalDigitsCount++;
+					}
+				}
+				if (equalDigitsCount == _n * _n)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		/// <summary>
+		/// Функция порождения потомков из указанного состояния
+		/// </summary>
+		/// <param name="position">List<List<int>></param>
+		/// <returns></returns>
+		public List<State> GenarateDescendants()
+		{
+			var result = new List<State>();
+			var emptyFieldPositon = GetEmptyPositionIndexes(this.Position);
+			var i = emptyFieldPositon.Item1;
+			var j = emptyFieldPositon.Item2;
+			var g = this.G + 1;
+
+			// Нижний правый угол
+			if (i == _n - 1 && j == _n - 1)
+			{
+				result.Add(MoveLeft(this, g, i, j));
+				result.Add(MoveUp(this, g, i, j));
+				return result;
+			}
+
+			// Нижный левый угол
+			if (i == _n - 1 && j == 0)
+			{
+				result.Add(MoveRight(this, g, i, j));
+				result.Add(MoveUp(this, g, i, j));
+				return result;
+			}
+
+			// Верхний левый угол
+			if (i == 0 && j == 0)
+			{
+				result.Add(MoveRight(this, g, i, j));
+				result.Add(MoveDown(this, g, i, j));
+				return result;
+			}
+
+			// Верхний правый угол
+			if (i == 0 && j == _n - 1)
+			{
+				result.Add(MoveLeft(this, g, i, j));
+				result.Add(MoveDown(this, g, i, j));
+				return result;
+			}
+
+			// Верхная строка (по середине)
+			if (i == 0)
+			{
+				result.Add(MoveRight(this, g, i, j));
+				result.Add(MoveLeft(this, g, i, j));
+				result.Add(MoveDown(this, g, i, j));
+				return result;
+			}
+
+			// Нижняя строка (по середине)
+			if (i == _n - 1)
+			{
+				result.Add(MoveRight(this, g, i, j));
+				result.Add(MoveLeft(this, g, i, j));
+				result.Add(MoveUp(this, g, i, j));
+				return result;
+			}
+
+			// Левый столбец (по середине)
+			if (j == 0)
+			{
+				result.Add(MoveRight(this, g, i, j));
+				result.Add(MoveDown(this, g, i, j));
+				result.Add(MoveUp(this, g, i, j));
+				return result;
+			}
+
+			// Правый столбец (по середине)
+			if (j == _n - 1)
+			{
+				result.Add(MoveDown(this, g, i, j));
+				result.Add(MoveLeft(this, g, i, j));
+				result.Add(MoveUp(this, g, i, j));
+				return result;
+			}
+
+			result.Add(MoveDown(this, g, i, j));
+			result.Add(MoveLeft(this, g, i, j));
+			result.Add(MoveUp(this, g, i, j));
+			result.Add(MoveRight(this, g, i, j));
+			return result;
+		}
+	}
+}
---
+++ BozhkoLab1/BozhkoLab1/Program.cs
@@ -0,0 +1,53 @@
+﻿using BozhkoLab1.Models;
+using System.Diagnostics.CodeAnalysis;
+
+var solution = new Tag();
+solution.FindSolution();
+
+class Tag
+{
+	private int H = 10000; 
+	private readonly int _maxIterationsNumber = 1000;
+	private List<State> Open { get; set; } = new();
+	private List<State> History { get; set; } = new();
+	private void Initialize()
+	{
+		var StartPosition = new State(0);
+		History.Add(StartPosition);
+		Open.Add(StartPosition);
+	}
+	private State FindStateViaMinimalF()
+	{
+		var minimalF = Open.Where(x => !x.IsOpened).Select(x => x.G + x.H).Min();
+		var stateViaMinimalF = Open.Where(x => !x.IsOpened).First(x => (x.G + x.H) == minimalF);
+		stateViaMinimalF.IsOpened = true;
+		return stateViaMinimalF;
+	}
+	public void FindSolution()
+	{
+		// Инициализировать начальное состояние
+		Initialize();
+
+		while(H > 1)
+		{
+			// Найти позицию с минимальной F, от которой надо порождать потомков
+			var stateViaMinimalF = FindStateViaMinimalF();
+			H = stateViaMinimalF.H;
+
+			// Породить всех потомков из текущего состояния
+			// Проверить, присутствуют ли они в Open
+			// Если не присутствуют - добавить в Open
+			var generatedDescendents = stateViaMinimalF.GenarateDescendants();
+			foreach (var generatedDescendent in generatedDescendents)
+			{
+				var isAlreadyOpen = generatedDescendent.IsAlreadyOpen(History.ToList());
+				if (!isAlreadyOpen)
+				{
+					History.Add(generatedDescendent);
+				}
+			}
+			Console.WriteLine($"H: {stateViaMinimalF.H}, G: {stateViaMinimalF.G}");
+		}
+	}
+}
+
